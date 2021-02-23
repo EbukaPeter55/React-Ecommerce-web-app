033af6be466e84ea1d030e6587be489e2456b28a
Ebuka <peterdbrainy5@gmail.com>
2021-02-23 17:23:58 +0100

    Displayed collections on the shop page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+
 
 function App() {
   return (
     <div>
-   <HomePage />
-    </div>
+    <Switch>
+    <Route exact path='/' component={HomePage} />
+    <Route path='/shop' component={ShopPage} />  
+    </Switch>
+       </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
