d438a99f2df4a506faa2302fbf028a5f77f800a0
Ebuka <peterdbrainy5@gmail.com>
2021-04-01 02:46:17 +0100

    Added a reusable custom button component and imported it to the signin

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
-// import CustomButton from '../custom-button/custom-button.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -51,6 +51,7 @@ class SignIn extends React.Component {
               label='password'
               required
             />            
+          <CustomButton type='submit'> Sign in </CustomButton>
           </form>
         </div>
       );
