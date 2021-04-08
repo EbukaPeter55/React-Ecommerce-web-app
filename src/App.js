5f2d4a99a96995e68d401f18562002f010224750
Ebuka <peterdbrainy5@gmail.com>
2021-04-08 17:43:06 +0100

    Connected the firebase auth service to the project and added a signout to the header navbar

--- src/App.js
+++ src/App.js
@@ -7,21 +7,43 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
 
+class App extends React.Component {
+constructor(){
+  super();
+  this.state =  {
+    currentUser: null
+  }
+}
 
-function App() {
-  return (
-    <div>
-    <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-       
-      </Switch>
-    </div>
-  );
+unsubscribeFromAuth = null
+
+componentDidMount(){
+  this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+    this.setState({currentUser: user});
+    console.log(user);
+  });
+}
+
+componentWillUnmount(){
+   this.unsubscribeFromAuth();
+}
+  render(){
+
+    return (
+      <div>
+      <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+         
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
