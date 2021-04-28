00c244e62484e21303dd389a4ab63adcee5be602
ebukapeter <peterdbrainy5@gmail.com>
2021-04-28 18:11:20 +0100

    Added cart item and cart dropdown componenets to the app and implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 
 // Using the React library special "ReactComponent as Logo to import SVG image."
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -29,14 +32,17 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
   );
   
   // state here represents the root reducer
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-}); 
-
+  const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
+  });
+  
 // connect helps to accept 2 functions, one of which is mapStateToProps. it connnects a react component to our redux store. 
 export default connect(mapStateToProps)(Header);//We now have access to the current user
