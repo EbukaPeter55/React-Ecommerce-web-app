5a28daabb34afd78a8d66d73d95052be579e62be
ebukapeter <peterdbrainy5@gmail.com>
2021-05-01 04:52:00 +0100

    Updated our application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,25 +2,31 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{itemCount}</span>
     </div>
   );
 
   const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
   });
+
+  // Using the selector to get just a slice or a new item based off the state
+  const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+  });
   
   export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(CartIcon);
   
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,20 @@
+import { createSelector } from 'reselect';
+
+// Input selector gets only a slice or what we need from the state up to a level deep
+const selectCart = state => state.cart;
+
+// Output selector using createSelector makes it a memoized selector
+export const selectCartItems = createSelector(
+    [selectCart],
+    cart => cart.cartItems
+);
+
+// New selector for countItems
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems =>
+    cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+      )
+)
+
