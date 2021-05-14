d89c13c63e2d97899ebbdc311a31398721c32f9a
ebukapeter <peterdbrainy5@gmail.com>
2021-05-14 17:05:16 +0100

    rectify the syntax error in the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import rootReducer from './root-reducer';
 
 const middlewares = [];
 
-if (process.env.NODE_env === 'development'){
+if (process.env.NODE_ENV === 'development'){
     middlewares.push(logger);
 }
 
