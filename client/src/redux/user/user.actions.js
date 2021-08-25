3aec7ae6e83474d5473c95ffff2e8e125f7ea61d
Ebuka Peter <peterdbrainy5@gmail.com>
2021-08-25 12:28:37 +0100

    didn't make any significant change to the user action

--- client/src/redux/user/user.actions.js
+++ client/src/redux/user/user.actions.js
@@ -1,4 +1,5 @@
-import UserActionTypes from './user.types';
+import UserActionTypes from '../user.types';
+
 
 export const googleSignInStart = () => ({
   type: UserActionTypes.GOOGLE_SIGN_IN_START
